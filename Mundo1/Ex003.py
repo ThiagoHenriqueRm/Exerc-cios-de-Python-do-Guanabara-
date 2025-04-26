14b6f12046e34e069cefa8453026b87669e75948
Thiago <139889189+ThiagoHenriqueRm@users.noreply.github.com>
2025-04-26 09:00:05 -0300

    Add files via upload

---
+++ Mundo1/Ex003.py
@@ -0,0 +1,9 @@
+# Python
+from os import system
+system("clear")
+
+n1 = int(input("Digite um numero : "))
+n2 = int(input("Digite outro numero : "))
+s  = n1 + n2
+
+print(f"\n {n1} + {n2} = {s} \n ")
