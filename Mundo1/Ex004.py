d59a664d7c4afc051a5260a78275531fce73e981
Thiago <139889189+ThiagoHenriqueRm@users.noreply.github.com>
2025-04-27 11:51:57 -0300

    Add files via upload

---
+++ Mundo1/Ex004.py
@@ -0,0 +1,28 @@
+# Python
+from os import system
+system("clear")
+
+entrada = input("\nEntrda : ")
+
+tipo        = type(entrada)
+ascii       = entrada.isascii()
+alfabetico  = entrada.isalpha()
+minuscula   = entrada.islower()
+maiuscula   = entrada.isupper()
+captalizada = entrada.istitle()
+alfanumerio = entrada.isalnum()
+numerico    = entrada.isnumeric()
+
+print(
+    f"\n INFORMAÇOES -> {entrada} \n",
+    
+    f"\n Tipo? ---------> : {tipo}",
+    f"\n Ascii? --------> : {ascii}",
+    f"\n Alfabetico? ---> : {alfabetico}",
+    f"\n Minuscula? ----> : {minuscula}",
+    f"\n Maiuscula? ----> : {maiuscula}",
+    f"\n Captalizada? --> : {captalizada}",
+    f"\n Alfanumerico? -> : {alfanumerio}",
+    f"\n Numerico? -----> : {numerico}",
+    "\n"
+)
