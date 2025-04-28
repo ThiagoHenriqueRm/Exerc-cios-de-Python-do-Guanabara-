89108f5444189707adf5b712e8d98a29bb2d7cf0
Th1ag0 <139889189+ThiagoHenriqueRm@users.noreply.github.com>
2025-04-28 10:44:31 -0300

    Update Ex005.py

--- Mundo1/Ex005.py
+++ Mundo1/Ex005.py
@@ -1,6 +1,5 @@
 # Python
-from os import system
-system("clear")
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
 
 n = int(input("Numero : "))
 print(f"\n {n - 1} <- {n} -> {n + 1} \n")
