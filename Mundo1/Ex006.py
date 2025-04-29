7649e707312906d94b5167fdea6e7a9c57e7e6c2
Th1ag0 <139889189+ThiagoHenriqueRm@users.noreply.github.com>
2025-04-29 12:43:53 -0300

    Update Ex006.py

--- Mundo1/Ex006.py
+++ Mundo1/Ex006.py
@@ -5,11 +5,11 @@ n = int(input("Numero : "))
 
 dobro  = n * 2
 triplo = n * 3
-rais   = n ** (1/2)
+raiz   = n ** (1/2)
 
 print(
     f"\n Dobro ---------> : {dobro}",
     f"\n Triplo --------> : {triplo}",
-    f"\n Raiz Quadrada -> : {rais:.2f}"
+    f"\n Raiz Quadrada -> : {raiz:.2f}"
     "\n"
 )
