47788a56836659a12cf53cf11806263efcc16be4
Th1ag0 <139889189+ThiagoHenriqueRm@users.noreply.github.com>
2025-04-30 08:28:15 -0300

    Update Ex007.py

--- Mundo1/Ex007.py
+++ Mundo1/Ex007.py
@@ -1,6 +1,5 @@
 # Python
-from os import system
-system("clear")
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 
 n1    = float(input("Nota 1 : ").replace(",", "."))
 n2    = float(input("Nota 2 : ").replace(",", "."))
