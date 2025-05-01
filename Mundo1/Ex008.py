bd13b861f3c3b595c88f929f3c2b71cb166cf78d
Th1ag0 <139889189+ThiagoHenriqueRm@users.noreply.github.com>
2025-05-01 12:02:00 -0300

    Update Ex008.py

--- Mundo1/Ex008.py
+++ Mundo1/Ex008.py
@@ -1,5 +1,5 @@
 # Python
-# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+# Escreva um programa que leia um valor em metros e o exiba convertido em : km , hm , dam , m , dm , cm e mm .
 
 m = float(input("Metros: ").replace(",", "."))
 
