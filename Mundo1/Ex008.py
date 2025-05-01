9718dc2957c68b5b05268a6659e95efacf5908aa
Th1ag0 <139889189+ThiagoHenriqueRm@users.noreply.github.com>
2025-05-01 11:19:07 -0300

    Add files via upload

---
+++ Mundo1/Ex008.py
@@ -0,0 +1,23 @@
+# Python
+from os import system
+system("clear")
+
+m = float(input("Metros : ").replace(",", "."))
+
+km  = m / 1000
+hm  = m / 100
+dam = m / 10
+dm  = m * 10
+cm  = m * 100
+mm  = m * 1000
+
+print(
+    f"\n Kilometros --> : {km}km   ",
+    f"\n Hequitros ---> : {hm}hm   ",
+    f"\n Decametros --> : {dam}dam ",
+    f"\n Metos -------> : {m}m     ",
+    f"\n Decimetros --> : {dm}dm   ",
+    f"\n Centimetros -> : {cm}cm   ",
+    f"\n Milimetros --> : {mm}mm   ",
+    "\n"
+)
