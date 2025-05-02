0b8845ca2567664be38ba685ba3ebf35dd112814
Th1ag0 <139889189+ThiagoHenriqueRm@users.noreply.github.com>
2025-05-02 11:44:10 -0300

    Add files via upload

---
+++ Mundo1/Ex009.py
@@ -0,0 +1,20 @@
+# Python
+# Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+
+n = int(input("Numero : "))
+
+print(
+    f"\n=+",
+    f"\n | {n} x  0 = {n*0:^3}",    
+    f"\n | {n} x  1 = {n*1:^3}",
+    f"\n | {n} x  2 = {n*2:^3}",
+    f"\n | {n} x  3 = {n*3:^3}",
+    f"\n | {n} x  4 = {n*4:^3}",
+    f"\n | {n} x  5 = {n*5:^3}",
+    f"\n | {n} x  6 = {n*6:^3}",
+    f"\n | {n} x  7 = {n*7:^3}",
+    f"\n | {n} x  8 = {n*8:^3}",
+    f"\n | {n} x  9 = {n*9:^3}",
+    f"\n | {n} x 10 = {n*10:^3}",
+     "\n=+"
+)
