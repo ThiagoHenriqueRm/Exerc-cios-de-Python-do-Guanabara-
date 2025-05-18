4a4aa9d56158bc576f3dab518ce8eddf49380902
Th1ag0 <139889189+ThiagoHenriqueRm@users.noreply.github.com>
2025-05-18 12:04:28 -0300

    Add files via upload

---
+++ Mundo1/Ex025.py
@@ -0,0 +1,7 @@
+# Python
+# Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+
+nome = str(input("Seu nome todo : ")).strip()
+ver = ("SILVA" in nome.upper())
+
+print(f"\n Tem Silva no nome ? : {ver}\n")
