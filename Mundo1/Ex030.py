7f8aebbb6504c0f53d67dd82ba422d269c67995e
ThiagoHenriqueRm <thiagohenriquerm583@gmail.com>
2025-06-29 15:54:27 -0300

    Add Ex030.py

---
+++ Mundo1/Ex030.py
@@ -0,0 +1,9 @@
+# Python
+# Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+n = int(input("Numeor : "))
+
+if( n % 2 == 0 ):
+    print(f"\n {n} é PAR! \n")
+else: 
+    print(f"\n {n} é IMPAR! \n")
