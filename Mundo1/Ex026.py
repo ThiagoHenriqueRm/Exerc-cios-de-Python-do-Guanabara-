4319e3b2a355f91085ed61f6116072cf5af963ff
ThiagoHenriqueRm <thiagohenriquerm583@gmail.com>
2025-06-29 15:22:51 -0300

    Add Ex026.py

---
+++ Mundo1/Ex026.py
@@ -0,0 +1,19 @@
+# Python
+# Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", 
+# E em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+
+from os import system
+system("clear")
+
+frase = str(input("Sua frasa : ")).upper().strip()
+
+contA     = frase.count("A")
+primairoA = frase.find("A")
+ultimoA   = frase.rfind("A")
+
+print(
+    f"\n lateas A -------------------> : {contA}",
+    f"\n 1° A está na posição -------> : {primairoA+1}",
+    f"\n Ultimo A está na posição ---> : {ultimoA}",
+    "\n"
+)
